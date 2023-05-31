c799856430d379c977d93e80cacd8cec563edc97
matias smoje <matias.smoje@gmail.com>
2023-05-31 00:13:26 -0400

    Client Socket + Pickles

--- client.py
+++ client.py
@@ -1,10 +1,14 @@
 # This client is designed to connect to manjarin hacklab :]
 import socket
+import sys
+import pickle
 
-HEADERSIZE=15
+port = int(sys.argv[1])
+
+HEADERSIZE=10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.1.99',2894))
+s.connect(('192.168.1.99',port))
 
 
 full_msg = ''
@@ -13,25 +17,28 @@ full_msg = ''
 
 
 while True:
-    full_msg = ''
+    full_msg = b''
     new_msg = True
 
     while True:
-        msg = s.recv(HEADERSIZE)
+        msg = s.recv(16)
         if new_msg:
-            print(f"new msg length: {msg[:HEADERSIZE]}")
+            print(f"NewMSGLen: {msg[:HEADERSIZE]}")
             msglen = int(msg[:HEADERSIZE])
             new_msg = False
 
-        full_msg += msg.decode("latin1")
-
+        full_msg += msg
+        
         if len(full_msg)-HEADERSIZE == msglen:
             print("FULL MSG RECVD")
             print(full_msg[HEADERSIZE:])
+
+            d = pickle.loads(full_msg[HEADERSIZE:])
+            print(d)
+
             new_msg = True
-            full_msg = ''
+            full_msg = b''
 
-    print(full_msg)
-
+    
 
 
